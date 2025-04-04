6590a14a1d3c5f903ecb773379220782d4374e82
Adrian Juel Boncodin <juelboncodin@gmail.com>
2025-04-04 20:17:47 +0800

    adding the jwt token at auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/usersModel');
+require('dotenv').config();
 
 exports.protect = async (req, res, next) => {
   let token;
@@ -11,10 +12,10 @@ exports.protect = async (req, res, next) => {
   ) {
     try {
       // Get the token from the header
-      token = req.headers.authorization.split(' ')[1];
+      token = req.headers.authorization.split('')[1];
 
       // Verify the token using JWT secret
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, '19b8b90ff1e827cf26b312121bea07d5e0e06d28a7cfc4ac9072b0257b87244a0ded59d59b954df78ae7233ff00c4667d69b5483115c87fd19a3da4127003f62');
 
       // Attach user info to the request object (without password)
       req.user = await User.findById(decoded.id).select('-password');
