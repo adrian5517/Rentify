f372c8feaa52bb8a1683ffc50a7f0f3b3b45a78c
Adrian Juel Boncodin <juelboncodin@gmail.com>
2025-04-04 19:00:37 +0800

    Updated the propertyModels

--- models/propertyModel.js
+++ models/propertyModel.js
@@ -1,26 +1,62 @@
 const mongoose = require('mongoose');
 
 const propertySchema = new mongoose.Schema({
-
-    ownerId :{type:mongoose.Schema.Types.ObjectId, ref:'User', required:true},
-    name:{type:String, required:true},
-    description:{ type:String , required:true},
-    address:{type:String, required:true},
-    city:{type:String, required:true},
-    province:{type:String , required:true},
-    zipCode:{type:String , required:true},
-    coordinates:{
-        latitude:{type:Number},
-        longitude:{type:Number}
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    price:{type:Number , required:true},
-    type:{type:String, enum:['house,apartment , studio unit , condo , boarding house'], required:true},
-    availableRooms:{type:Number},
-    amenities:[{type:String}],
-    status:{type:String , enum:['Available', 'Occupied', 'Under Maintenance'], default:'Available'},
-    images:[{type:String}],
-    
-    
-    },{timestamps:true});
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    province: {
+        type: String,
+        required: true
+    },
+    zipCode: {
+        type: String,
+        required: true
+    },
+    coordinates: {
+        latitude: { type: Number, default: null },
+        longitude: { type: Number, default: null }
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['house', 'apartment', 'studio unit', 'condo', 'boarding house'],
+        required: true
+    },
+    availableRooms: {
+        type: Number
+    },
+    amenities: [{
+        type: String
+    }],
+    status: {
+        type: String,
+        enum: ['Available', 'Occupied', 'Under Maintenance'],
+        default: 'Available'
+    },
+    images: [{
+        type: String
+    }]
+}, { timestamps: true });
 
-    module.exports = mongoose.model('Property', propertySchema);
+module.exports = mongoose.model('Property', propertySchema);
