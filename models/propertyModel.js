a242a078d6a5c0eb0a34a34301abe43ae43c4cc9
Adrian Juel Boncodin <juelboncodin@gmail.com>
2025-04-04 22:30:54 +0800

    Editing property model added require

--- models/propertyModel.js
+++ models/propertyModel.js
@@ -2,26 +2,26 @@ const mongoose = require('mongoose');
 
 const propertySchema = new mongoose.Schema({
   name: {
-    type: String,
-    required: true,
+    type: String,required: true, // Property name
+    
   },
   address: {
-    type: String,
-    required: true,
+    type: String, required: true, // Property address
+    
   },
   price: {
-    type: Number,
-    required: true,
+    type: Number, required: true, // Property price per night
+    
   },
   description: {
-    type: String,
-    required: true,
+    type: String,required: true, // Property description
+    
   },
   images: [String], // Array of image URLs
   ownerId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Reference to the User model (assuming you have a User model)
-    required: true, // You can set this to true if every property must have an owner
+    ref: 'User', // Reference to the User model (assuming you have a User model) // You can set this to true if every property must have an owner
+    required: true, // Owner ID is required
   },
 }, {
   timestamps: true, // Automatically add createdAt and updatedAt fields
