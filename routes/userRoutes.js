bff480b06ea04a16f739c532606ea7e288d88d2d
Adrian Juel Boncodin <juelboncodin@gmail.com>
2025-04-20 14:08:35 +0800

    Adding waste MVC

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUsers, getUserById, updateUser, deleteUser, loginUser } = require('../controllers/userController');
-// const auth = require('../middleware/authMiddleware');  // Protect routes that need authentication
+const { getUsers, getUserById, updateUser, deleteUser } = require('../controllers');
+const auth = require('../middleware/authMiddleware');  // Protect routes that need authentication
 
 
 
 // Protected routes (Requires Authentication)
-router.get('/',  getUsers);  // Get all users
-router.get('/:id', getUserById);  // Get user by ID
-router.put('/:id', updateUser);  // Update user details
-router.delete('/:id', deleteUser);  // Delete user
+router.get('/', auth, getUsers);  // Get all users
+router.get('/:id', auth, getUserById);  // Get user by ID
+router.put('/:id', auth, updateUser);  // Update user details
+router.delete('/:id', auth, deleteUser);  // Delete user
 
 module.exports = router;
