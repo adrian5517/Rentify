92ac2927c2910c4ac99a8a6f90a4b4b080f70d23
Adrian Juel Boncodin <juelboncodin@gmail.com>
2025-04-23 15:51:28 +0800

    Adding Kmeans Algo. at Properties MVC

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -5,7 +5,6 @@ const propertyController = require('../controllers/propertyController');
 router.get('/', propertyController.getAllProperties);
 router.get('/:id', propertyController.getPropertyById);
 router.post('/', propertyController.createProperty);
-router.post('/bulk', propertyController.createBulkProperties); // 🔥 Bulk insert
 router.put('/:id', propertyController.updateProperty);
 router.delete('/:id', propertyController.deleteProperty);
 
