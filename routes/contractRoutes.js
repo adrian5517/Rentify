75a043251047f0a108fe3a7e69a93e6c87e1af19
adrian5517 <juelboncodin@gmail.com>
2026-01-22 08:34:10 +0800

    fixContractRoutes

--- routes/contractRoutes.js
+++ routes/contractRoutes.js
@@ -6,14 +6,16 @@ const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', protect, controller.createContract);
 router.get('/user/:userId', protect, controller.listContractsByUser);
+// Current user contracts - must be declared before '/:id' to avoid 'me' being treated as an id
+router.get('/me', protect, controller.listContractsByUser);
+
 router.get('/:id', protect, controller.getContract);
 router.put('/:id', protect, controller.updateContract);
 router.post('/:id/docs', protect, upload.array('docs', 5), controller.uploadContractDocument);
 router.post('/:id/accept', protect, controller.acceptContract);
 router.post('/:id/propose-edit', protect, controller.proposeContractEdit);
 router.get('/property/:propertyId', protect, controller.listContractsByProperty);
-// Current user contracts
-router.get('/me', protect, controller.listContractsByUser);
+// (other routes remain)
 
 const pdfController = require('../controllers/pdfController')
 console.log('pdfController in routes:', pdfController);
