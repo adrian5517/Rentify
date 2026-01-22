a9753fd4db25aa3a29d1c120f918b9fbbc70393e
adrian5517 <juelboncodin@gmail.com>
2026-01-22 10:00:08 +0800

    debugged contactRoutes

--- routes/contractRoutes.js
+++ routes/contractRoutes.js
@@ -18,9 +18,6 @@ router.get('/property/:propertyId', protect, controller.listContractsByProperty)
 // (other routes remain)
 
 const pdfController = require('../controllers/pdfController')
-console.log('pdfController in routes:', pdfController);
-console.log('generateContractPdf in routes:', pdfController.generateContractPdf);
-// Restored protect middleware on the PDF route
 router.get('/:id/pdf', protect, pdfController.generateContractPdf);
 
 module.exports = router;
